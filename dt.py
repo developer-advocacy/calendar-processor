3ebbf676970860ff5d5a3f71e2a42078d59a955e
Josh Long <josh@joshlong.com>
2021-10-16 03:44:29 -0700

    calendars ftw

---
+++ dt.py
@@ -0,0 +1,74 @@
+import datetime
+import os
+import typing
+
+
+def get_current_month_dates() -> typing.Tuple[datetime.datetime, datetime.datetime]:
+    now = datetime.datetime.now()
+    month = now.month
+    year = now.year
+
+    def calculate_next_month(y: int, m: int) -> typing.Tuple[int, int]:
+        if m == 12:
+            return y + 1, 1
+        return y, m + 1
+
+    start_date = datetime.datetime(year=year, month=month, day=1)
+    next_year, next_month = calculate_next_month(year, month)
+    stop_date = datetime.datetime(year=next_year, month=next_month, day=1)
+    return start_date, stop_date
+
+
+def parse_time(time: str) -> str:
+    return __pad_time(__normalize_times(time))
+
+
+def __pad_time(time: str) -> str:
+    if time is None:
+        return None
+
+    def pad_digit(d: str) -> str:
+        if len(d) == 1:
+            return f'0{d}'
+
+    if len(time) == 4:
+        return time
+    if ':' in time:
+        l, r = time.split(':')
+        l = pad_digit(l)
+        r = pad_digit(r)
+        return f'{l}:{r}'
+    else:
+        return f'{time}:00'
+
+
+def __normalize_times(time: str) -> str:
+    if time is None or time == '':
+        return None
+    assert time is not None, 'the time must be non-empty'
+    time = time.lower().strip()
+    if 'noon' in time:
+        return '12:00'
+    if 'midnite' in time:
+        return '00:00'
+    has_numbers = time[:]
+    has_numbers = any([a.isdigit() for a in has_numbers])
+    if not has_numbers:
+        return '00:00'
+    keep = ''
+    found_number = False
+    for c in time[:]:
+        is_digit = c.isdigit() or c == ':'
+        if is_digit:
+            found_number = True
+            keep += c
+        else:
+            if found_number:
+                break
+    keep = keep.strip()
+    parts = keep.split(':')
+    parts = [int(x.strip()) for x in parts]
+    if len(parts) == 2:
+        return f'{parts[0]}:{parts[1]}'
+    elif len(parts) == 1:
+        return f'{parts[0]}'
--- main.py
+++ main.py
@@ -1,9 +1,26 @@
 #!/usr/bin/env python
 import datetime
 import os
+
 import pytz
+
 import auth
+import dt
 from google import sheets, calendar
+
+
+class Event(object):
+
+    def __str__(self) -> str:
+        return f'start: {self.start} stop: {self.stop} time: {self.time} event: "{self.event}" '
+
+    def __init__(self, event: str, start: datetime.datetime, stop: datetime.datetime, time: str) -> None:
+        super().__init__()
+        self.event: str = event
+        self.start: datetime.datetime = start
+        self.stop: datetime.datetime = stop
+        self.time: str = dt.parse_time(time)
+
 
 def main():
     scopes: list = ['https://www.googleapis.com/auth/drive', 'https://www.googleapis.com/auth/calendar']
@@ -12,24 +29,34 @@ def main():
                                     os.path.join(desktop, 'authenticated-token.json'),
                                     scopes)
     assert credentials is not None, 'the credentials must be valid!'
-    my_sheet: sheets.GoogleSheet = sheets.GoogleSheet(credentials, os.environ['SHEET_ID'])
+    sheet_id = os.environ['SHEET_ID']
+    my_sheet: sheets.GoogleSheet = sheets.GoogleSheet(credentials, sheet_id)
     my_calendar: calendar.GoogleCalendar = calendar.GoogleCalendar(credentials)
     tz = pytz.timezone('America/Los_Angeles')
-    start = datetime.datetime(year=2021, month=10, day=15, hour=17, minute=33, second=32)
-    stop = datetime.datetime(year=2021, month=10, day=15, hour=19, minute=2, second=0)
+    start_date, stop_date = dt.get_current_month_dates()
+    rows = my_sheet.read_values('A:X')
+    event_header = find_column(rows, 'event')
+    start_date_header = find_column(rows, 'start date')
+    stop_date_header = find_column(rows, 'end date')
+    time_header = find_column(rows, 'time')
+    cols = [time_header, event_header, start_date_header, stop_date_header]
+    col_width = max(cols) + 1
+    rows = [r for r in rows[1:]]
+    rows = [r for r in rows if len(r) >= col_width]
+    rows = [(row[event_header], row[start_date_header], row[stop_date_header], row[time_header]) for row in rows]
+    rows = [Event(event=x[0], start=x[1], stop=x[2], time=x[3]) for x in rows]
 
-    if True:
-        for i in range(2):
-            new_event = my_calendar.create_event(
-                summary='a test event', location='online', description='this will be a fun thing we do',
-                stopdatetime=stop, startdatetime=start, tz=tz
-            )
-            print(f"the new event link: {new_event.get('htmlLink')}")
+    for row in rows:
+        print(row)
 
-    for e in my_calendar.get_events_between(start, stop, tz):
-        print('-' * 10)
-        print(e)
-        my_calendar.delete_event(eventid=e['id'])
+
+def find_column(rows: list[object], name: str) -> int:
+    ctr = 0
+    for c in rows[0]:
+        if c.lower() == name.lower():
+            return ctr
+        ctr += 1
+    return -1
 
 
 if __name__ == '__main__':
