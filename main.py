60ab196de8e8c33ed68e8679f53f1ceabd480620
Josh Long <josh@joshlong.com>
2021-10-15 17:31:43 -0700

    calendars ftw

--- main.py
+++ main.py
@@ -19,11 +19,13 @@ def main():
     tz = pytz.timezone('America/Los_Angeles')
     start = datetime.datetime(year=2021, month=10, day=15, hour=17, minute=33, second=32)
     stop = datetime.datetime(year=2021, month=10, day=15, hour=19, minute=2, second=0)
-    new_event = my_calendar.create_event(summary='a test event', location='online',
-                                         description='this will be a fun thing we do',
-                                         stopdatetime=stop, startdatetime=start,
-                                         tz=tz)
-    print(f"the new event link: {new_event.get('htmlLink')}")
+
+    if False:
+        new_event = my_calendar.create_event(
+            summary='a test event', location='online', description='this will be a fun thing we do',
+            stopdatetime=stop, startdatetime=start, tz=tz
+        )
+        print(f"the new event link: {new_event.get('htmlLink')}")
     for e in my_calendar.get_events_between(start, stop, tz):
         print('-' * 10)
         print(e)
