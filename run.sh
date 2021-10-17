4999c5842664f6e8b005f136952ca7c22c657da4
Josh Long <josh@joshlong.com>
2021-10-17 01:23:53 -0700

    pytz

--- run.sh
+++ run.sh
@@ -8,8 +8,5 @@ export AUTHENTICATED_CREDENTIALS_JSON_FN=$HOME/authenticated-credentials.json
 echo "${AUTHENTICATED_CREDENTIALS_JSON}" | base64 -d > $AUTHENTICATED_CREDENTIALS_JSON_FN
 echo "${CREDENTIALS_JSON}" | base64 -d > $CREDENTIALS_JSON_FN
 
-ls -la ${CREDENTIALS_JSON_FN}
-ls -la ${AUTHENTICATED_CREDENTIALS_JSON_FN}
-
 pipenv install
 pipenv run python main.py
